1472c5cbd0154dd9f789efcb473d555e74e65782
mondothecoder <mwansamondo068@gmail.com>
2023-05-22 12:35:34 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed=new typed(".multiple-text", {
+    strings: ["frontend Developer", "Junior SoftwareEngineer",
+     "PHP Developer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+
+} )
